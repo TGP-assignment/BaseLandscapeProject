2197dc82b7eb5b55abcf5dcbd675783d8c33f173
unknown <g016154e@K106-20.student.staffs.ac.uk>
2015-12-01 14:31:40 +0000

    Readying merge with master - preparing to split repo with Neil

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -34,6 +34,34 @@ bool HelloWorld::init()
     auto rootNode = CSLoader::createNode("MainScene.csb");
 
     addChild(rootNode);
+	downSpeed = 0;
+	startXpos = 0.0;
+	startYpos = 700.0;
+
+	bean_1 = (Sprite*)rootNode->getChildByName("bean_1");
+	bean_2 = (Sprite*)rootNode->getChildByName("bean_2");
+	bean_3 = (Sprite*)rootNode->getChildByName("bean_3");
+	bean_4 = (Sprite*)rootNode->getChildByName("bean_4");
+	cane = (Sprite*)rootNode->getChildByName("cane");
+	corn = (Sprite*)rootNode->getChildByName("corn");
+	heart_1 = (Sprite*)rootNode->getChildByName("heart_1");
+	heart_2 = (Sprite*)rootNode->getChildByName("heart_2");
+	heart_3 = (Sprite*)rootNode->getChildByName("heart_3");
+	heart_4 = (Sprite*)rootNode->getChildByName("heart_4");
+	jelly_1 = (Sprite*)rootNode->getChildByName("jelly_1");
+	jelly_2 = (Sprite*)rootNode->getChildByName("jelly_2");
+	jelly_3 = (Sprite*)rootNode->getChildByName("jelly_3");
+	jelly_4 = (Sprite*)rootNode->getChildByName("jelly_4");
+	wrapped_1 = (Sprite*)rootNode->getChildByName("wrapped_1");
+	wrapped_2 = (Sprite*)rootNode->getChildByName("wrapped_2");
+	wrapped_3 = (Sprite*)rootNode->getChildByName("wrapped_3");
+	wrapped_4 = (Sprite*)rootNode->getChildByName("wrapped_4");
 
     return true;
+}
+
+void HelloWorld::update()
+{
+	auto  winSize = Director::getInstance()->getVisibleSize();
+
 }
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -14,6 +14,29 @@ public:
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+	void update();
+
+	cocos2d::Sprite*        bean_1;
+	cocos2d::Sprite*        bean_2;
+	cocos2d::Sprite*        bean_3;
+	cocos2d::Sprite*        bean_4;
+	cocos2d::Sprite*        cane;
+	cocos2d::Sprite*        corn;
+	cocos2d::Sprite*        heart_1;
+	cocos2d::Sprite*        heart_2;
+	cocos2d::Sprite*        heart_3;
+	cocos2d::Sprite*        heart_4;
+	cocos2d::Sprite*        jelly_1;
+	cocos2d::Sprite*        jelly_2;
+	cocos2d::Sprite*        jelly_3;
+	cocos2d::Sprite*        jelly_4;
+	cocos2d::Sprite*        wrapped_1;
+	cocos2d::Sprite*        wrapped_2;
+	cocos2d::Sprite*        wrapped_3;
+	cocos2d::Sprite*        wrapped_4;
+	int		downSpeed;
+	float		startXpos;
+	float		startYpos;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
